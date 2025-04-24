5eb169ca78578bdbe7dada2b4bae2501ca7c8e9a
GiovanniBombardieri <g.bombardieri06@gmail.com>
2025-04-24 20:03:59 +0200

    🎨 Update input element of Login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -8,6 +8,7 @@ const Login = () => {
   const { login } = useAuth();
   const navigate = useNavigate();
   const [erroreLogin, setErroreLogin] = useState("");
+  const [loginFailed, setLoginFailed] = useState(false);
 
   const handleLogin = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -75,6 +76,7 @@ const Login = () => {
       } else {
         setErroreLogin("Errore sconosciuto.");
       }
+      setLoginFailed(true);
     }
   };
 
@@ -87,25 +89,79 @@ const Login = () => {
         <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4 w-1/4 flex flex-col items-center">
           <legend className="fieldset-legend">Login</legend>
 
-          <label className="label">Email</label>
-          <input
-            type="email"
-            className="input text-center w-3/4"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            required
-          />
+          {/** CAMPO EMAIL */}
+          <label
+            className={`input w-3/4 text-center ${
+              loginFailed ? "border-red-600" : ""
+            }`}
+          >
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <rect width="20" height="16" x="2" y="4" rx="2"></rect>
+                <path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path>
+              </g>
+            </svg>
+            <input
+              type="email"
+              placeholder="Email"
+              className="input text-center"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </label>
+          <div className="validator-hint hidden">Enter valid email address</div>
 
-          <label className="label">Password</label>
-          <input
-            type="password"
-            className="input text-center w-3/4"
-            placeholder="Password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          {/** CAMPO PASSWORD */}
+          <label
+            className={`input w-3/4 text-center ${
+              loginFailed ? "border-red-600" : ""
+            }`}
+          >
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <path d="M2.586 17.414A2 2 0 0 0 2 18.828V21a1 1 0 0 0 1 1h3a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h1a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1h.172a2 2 0 0 0 1.414-.586l.814-.814a6.5 6.5 0 1 0-4-4z"></path>
+                <circle cx="16.5" cy="7.5" r=".5" fill="currentColor"></circle>
+              </g>
+            </svg>
+            <input
+              type="password"
+              className="input text-center"
+              placeholder="Password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}"
+              title="Must be more than 8 characters, including number, lowercase letter, uppercase letter"
+            />
+          </label>
+          <p className="validator-hint hidden">
+            Must be more than 8 characters, including
+            <br />
+            At least one number <br />
+            At least one lowercase letter <br />
+            At least one uppercase letter
+          </p>
 
           <button type="submit" className="btn btn-neutral my-4 w-3/4">
             Login
