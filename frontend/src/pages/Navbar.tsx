35c868a68a8c29c79e799e0e34e104ec73920b4e
GiovanniBombardieri <g.bombardieri06@gmail.com>
2025-06-06 08:35:50 +0200

    🎨 change for responsive dashboard

--- frontend/src/pages/Navbar.tsx
+++ frontend/src/pages/Navbar.tsx
@@ -128,7 +128,7 @@ const Navbar = () => {
                   <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78L12 21.23l8.84-8.84a5.5 5.5 0 0 0 0-7.78z" />
                 </svg>
 
-                <span className="badge badge-xs badge-primary indicator-item"></span>
+                {/* <span className="badge badge-xs badge-primary indicator-item"></span> */}
               </div>
             </button>
           )}
